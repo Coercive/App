39c7e8c9d24dd23a57f0383f78bbae7b2db0cb1e
Anthony <anthony@aouka.com>
2017-11-12 00:43:46 +0100

    container maison

---
+++ Factory/Container.php
@@ -0,0 +1,68 @@
+<?php
+namespace Coercive\App\Factory;
+
+use ArrayAccess;
+
+/**
+ * Container
+ *
+ * @package	Coercive\App\Factory
+ * @author	Anthony Moral <contact@coercive.fr>
+ */
+class Container implements ArrayAccess {
+
+	/** @var array Array access keys list */
+	private $_aKeys = [];
+
+	/** @var array Array access values list */
+	private $_aValues = [];
+
+	/**
+	 * @inheritdoc
+	 * @see ArrayAccess::offsetExists
+	 *
+	 * @param mixed $offset
+	 * @return bool
+	 */
+	public function offsetExists($offset) {
+		return isset($this->keys[$offset]);
+	}
+
+	/**
+	 * @inheritdoc
+	 * @see ArrayAccess::offsetGet
+	 *
+	 * @param mixed $offset
+	 * @return mixed|null
+	 */
+	public function offsetGet($offset) {
+		return isset($this->keys[$offset]) ? $this->_aValues[$offset] : null;
+	}
+
+	/**
+	 * @inheritdoc
+	 * @see ArrayAccess::offsetSet
+	 *
+	 * @param mixed $offset
+	 * @param mixed $value
+	 * @return void
+	 */
+	public function offsetSet($offset, $value) {
+		$this->_aValues[$offset] = $value;
+		$this->_aKeys[$offset] = true;
+	}
+
+	/**
+	 * @inheritdoc
+	 * @see ArrayAccess::offsetUnset
+	 *
+	 * @param mixed $offset
+	 * @return void
+	 */
+	public function offsetUnset($offset) {
+		if (isset($this->_aKeys[$offset])) {
+			unset($this->_aValues[$offset], $this->_aKeys[$offset]);
+		}
+	}
+
+}
