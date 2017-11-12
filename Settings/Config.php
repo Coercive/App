53e1d4ae4fc8b681f839a8dcc503ef2435339838
Anthony <anthony@aouka.com>
2017-11-12 01:09:18 +0100

    config public directory

--- Settings/Config.php
+++ Settings/Config.php
@@ -9,4 +9,30 @@ namespace Coercive\App\Settings;
  */
 class Config {
 
+	# PROPERTIES LIST
+	const PUBLIC_DIRECTORY = 'PUBLIC_DIRECTORY';
+
+	/** @var array Configuration datas */
+	private $_aDatas = [];
+
+	/**
+	 * SETTER : PUBLIC DIRECTORY
+	 *
+	 * @param string $sFullPath
+	 * @return $this
+	 */
+	public function setPublicDirectory($sFullPath) {
+		$this->_aDatas[self::PUBLIC_DIRECTORY] = (string) $sFullPath;
+		return $this;
+	}
+
+	/**
+	 * GETTER : PUBLIC DIRECTORY
+	 *
+	 * @return string
+	 */
+	public function getPublicDirectory() {
+		return $this->_aDatas[self::PUBLIC_DIRECTORY] ?? '';
+	}
+
 }
