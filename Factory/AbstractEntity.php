d51a79058b1f6d8ffad0f0f769eedeba15ec91e5
Coercive <contact@coercive.fr>
2018-04-11 09:56:09 +0200

    convention

--- Factory/AbstractEntity.php
+++ Factory/AbstractEntity.php
@@ -12,43 +12,44 @@ use Coercive\App\Settings\EntityInterface;
  * @link		https://github.com/Coercive/App
  *
  * @author  	Anthony Moral <contact@coercive.fr>
- * @copyright   2017 - 2018 Anthony Moral
+ * @copyright   2018 Anthony Moral
  * @license 	http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
  */
-abstract class AbstractEntity extends Container implements EntityInterface {
-
+abstract class AbstractEntity extends Container implements EntityInterface
+{
 	/**
 	 * AbstractEntity Constructor.
 	 *
-	 * @param array $aDatas [optional]
+	 * @param array $datas [optional]
 	 * @param AbstractApp $app [optional]
 	 */
-	abstract public function __construct(array $aDatas = [], AbstractApp $app = null);
+	abstract public function __construct(array $datas = [], AbstractApp $app = null);
 
 	/**
 	 * Entities Collection
 	 *
-	 * @param array $aDatas [optional]
+	 * @param array $datas [optional]
 	 * @param AbstractApp $app [optional]
 	 * @return array
 	 */
-	static public function Collection(array $aDatas = [], AbstractApp $app = null) {
-		foreach ($aDatas as $iKey => $aRowDatas) {
-			$aDatas[$iKey] = new static($aRowDatas, $app);
+	static public function Collection(array $datas = [], AbstractApp $app = null): array
+	{
+		foreach ($datas as $key => $item) {
+			$datas[$key] = new static($item, $app);
 		}
-		return $aDatas;
+		return $datas;
 	}
 
 	/**
 	 * Auto set datas in entity
 	 *
-	 * @param array $aDatas
+	 * @param array $datas
 	 * @return void
 	 */
-	protected function _autoSet(array $aDatas) {
-		foreach ($aDatas as $sFieldName => $mValue) {
-			$this->offsetSet($sFieldName, $mValue);
+	protected function autoSet(array $datas)
+	{
+		foreach ($datas as $field => $value) {
+			$this->offsetSet($field, $value);
 		}
 	}
-
 }
