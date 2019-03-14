75d3a310dd319073663cd1aacbc08f03095cc480
Coercive <contact@coercive.fr>
2019-03-14 15:08:13 +0100

    new container + iterator

--- Service/Container.php
+++ Service/Container.php
@@ -1,9 +1,12 @@
 <?php
 namespace Coercive\App\Service;
 
-use Iterator;
+use Closure;
 use Countable;
 use ArrayAccess;
+use Traversable;
+use ArrayIterator;
+use IteratorAggregate;
 
 /**
  * Container
@@ -15,13 +18,72 @@ use ArrayAccess;
  * @copyright 2019 Anthony Moral
  * @license MIT
  */
-class Container implements ArrayAccess, Iterator, Countable
+class Container implements ArrayAccess, Countable, IteratorAggregate
 {
 	/** @var array Array access values list */
 	private $array = [];
 
 	/** @var array Array access processed status */
 	private $prepared = [];
+
+	/**
+	 * Prepare Closures
+	 *
+	 * @return $this
+	 */
+	protected function prepare()
+	{
+		foreach ($this->array as $offset => $value)
+		{
+			# Detect closure or invoke
+			if (isset($this->prepared[$offset])
+				|| !is_object($this->array[$offset])
+				|| !method_exists($this->array[$offset], '__invoke')
+			) {
+				continue;
+			}
+
+			# Invoke
+			$this->array[$offset] = $this->array[$offset]($this);
+			$this->prepared[$offset] = true;
+		}
+		return $this;
+	}
+
+	/**
+	 * Replace actual content by new data
+	 *
+	 * @param array $data
+	 * @return $this
+	 */
+	public function from(array $data)
+	{
+		$this->clear();
+		$this->array = $data;
+		return $this;
+	}
+
+	/**
+	 * Empty internal array
+	 *
+	 * @return $this
+	 */
+	public function clear()
+	{
+		$this->array = [];
+		$this->prepared = [];
+		return $this;
+	}
+
+	/**
+	 * Clean empty values
+	 *
+	 * @return $this
+	 */
+	public function clean()
+	{
+		return $this->from(array_filter($this->array));
+	}
 
 	/**
 	 * Datalist for debug
@@ -36,12 +98,22 @@ class Container implements ArrayAccess, Iterator, Countable
 	/**
 	 * (GET) Property mode
 	 *
-	 * @param string $offset
+	 * @param mixed $offset
 	 * @return mixed
 	 */
-	public function __get(string $offset)
+	public function __get($offset)
 	{
 		return $this->offsetGet($offset);
+	}
+
+	/**
+	 * Alias of array copy
+	 *
+	 * @return array
+	 */
+	public function __invoke(): array
+	{
+		return $this->getArrayCopy();
 	}
 
 	/**
@@ -55,12 +127,97 @@ class Container implements ArrayAccess, Iterator, Countable
 	}
 
 	/**
+	 * Convert array to json string
+	 *
+	 * @return string
+	 */
+	public function __toString()
+	{
+		return $this->serialize();
+	}
+
+	/**
+	 * Convert array to json string
+	 *
+	 * @return string
+	 */
+	public function __toJson(): string
+	{
+		$this->prepare();
+		return json_encode($this->array);
+	}
+
+	/**
+	 * Export the object
+	 *
+	 * @return string
+	 */
+	public function serialize()
+	{
+		$this->prepare();
+		return serialize($this->array);
+	}
+
+	/**
+	 * Prepare the object with serialized datas
+	 *
+	 * @param string $serialized
+	 * @return $this
+	 */
+	public function unserialize(string $serialized)
+	{
+		return $this->from(unserialize($serialized));
+	}
+
+	/**
+	 * Convert array values to string
+	 *
+	 * @param string $separator
+	 * @return string
+	 */
+	public function join(string $separator = ','): string
+	{
+		$this->prepare();
+		return implode($separator, $this->array);
+	}
+
+	/**
+	 * Remove by value
+	 *
+	 * @param mixed $value
+	 * @param bool $strict
+	 * @return $this
+	 */
+	public function remove($value, bool $strict = true)
+	{
+		$offset = $this->indexOf($value, $strict);
+		if(null !== $offset) {
+			$this->offsetUnset($offset);
+		}
+		return $this;
+	}
+
+	/**
+	 * Slice internal array
+	 *
+	 * @param int $offset
+	 * @param int $length [optional]
+	 * @return Container
+	 */
+	public function slice(int $offset = 0, int $length = null)
+	{
+		if (null === $length) { $length = $this->count(); }
+		return $this->from(array_slice($this->array, $offset, $length));
+	}
+
+	/**
 	 * Sort the entries by value
 	 *
 	 * @return $this
 	 */
 	public function asort()
 	{
+		$this->prepare();
 		asort($this->array);
 		return $this;
 	}
@@ -73,6 +230,54 @@ class Container implements ArrayAccess, Iterator, Countable
 	public function ksort()
 	{
 		ksort($this->array);
+		return $this;
+	}
+
+	/**
+	 * Reverse the entries order
+	 *
+	 * @return $this
+	 */
+	public function reverse()
+	{
+		return $this->from(array_reverse($this->array, true));
+	}
+
+	/**
+	 * Get the index of given value
+	 *
+	 * @param mixed $value
+	 * @param bool $strict
+	 * @return int|string|null
+	 */
+	public function indexOf($value, bool $strict = true)
+	{
+		$this->prepare();
+		$index = array_search($value, $this->array, $strict);
+		return $index !== false ? $index : null;
+	}
+
+	/**
+	 * Detect if array contain the given value
+	 *
+	 * @param mixed $value
+	 * @param bool $strict [optional]
+	 * @return bool
+	 */
+	public function contains($value, bool $strict = true): bool
+	{
+		return in_array($value, $this->array, $strict);
+	}
+
+	/**
+	 * Append new entrie
+	 *
+	 * @param array $array
+	 * @return $this
+	 */
+	public function append(array $array)
+	{
+		array_push($this->array, $array);
 		return $this;
 	}
 
@@ -167,6 +372,17 @@ class Container implements ArrayAccess, Iterator, Countable
 
 	/**
 	 * @inheritdoc
+	 * @see IteratorAggregate::getIterator
+	 *
+	 * @return ArrayIterator|Traversable
+	 */
+	public function getIterator()
+	{
+		return new ArrayIterator($this->array);
+	}
+
+	/**
+	 * @inheritdoc
 	 * @see Countable::count
 	 *
 	 * @return int
@@ -177,57 +393,66 @@ class Container implements ArrayAccess, Iterator, Countable
 	}
 
 	/**
-	 * @inheritdoc
-	 * @see Iterator::current
+	 * Foreach callback
 	 *
-	 * @return mixed
+	 * @param Closure $function
+	 * @return $this
 	 */
-	public function current()
+	public function each(Closure $function)
 	{
-		return current($this->array);
+		foreach ($this->array as $key => $value)
+		{
+			$function($value, $key, $this);
+		}
+		return $this;
 	}
 
 	/**
-	 * @inheritdoc
-	 * @see Iterator::next
+	 * Filter callback
 	 *
-	 * @return mixed
+	 * @param Closure $function
+	 * @param bool $preserveKeys
+	 * @return $this
 	 */
-	public function next()
+	public function filter(Closure $function, bool $preserveKeys = false)
 	{
-		return next($this->array);
+		$results = [];
+		foreach ($this->array as $key => $value)
+		{
+			if ($function($value, $key, $this)) {
+				$preserveKeys ? $results[$key] = $value : $results[] = $value;
+			}
+		}
+		return $this->from($results);
 	}
 
 	/**
-	 * @inheritdoc
-	 * @see Iterator::key
+	 * Every callback
 	 *
-	 * @return mixed
-	 */
-	public function key()
-	{
-		return key($this->array);
-	}
-
-	/**
-	 * @inheritdoc
-	 * @see Iterator::valid
-	 *
+	 * @param Closure $function
 	 * @return bool
 	 */
-	public function valid(): bool
+	public function every(Closure $function): bool
 	{
-		return key($this->array) !== null;
+		foreach($this as $key => $value)
+		{
+			if (!$function($value, $key, $this)) { return false; }
+		}
+		return true;
 	}
 
 	/**
-	 * @inheritdoc
-	 * @see Iterator::rewind
+	 * Some callback
 	 *
-	 * @return void
+	 * @param Closure $function
+	 * @return bool
 	 */
-	public function rewind()
+	public function some(Closure $function): bool
 	{
-		reset($this->array);
+		foreach($this as $key => $value)
+		{
+			if ($function($value, $key, $this)) { return true; }
+		}
+		return false;
 	}
 }
