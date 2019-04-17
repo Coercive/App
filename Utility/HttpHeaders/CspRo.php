f0faaf46a9fcaddb10818c2e9772ca9fff5052bc
Anthony <anthony@aouka.com>
2019-04-17 18:09:12 +0200

    cspro doc link

--- Utility/HttpHeaders/CspRo.php
+++ Utility/HttpHeaders/CspRo.php
@@ -4,7 +4,7 @@ namespace Coercive\App\Utility\HttpHeaders;
 /**
  * Content Security Policy Report Only
  *
- * @see https://developer.mozilla.org/fr/docs/Web/HTTP/Headers/Content-Security-Policy
+ * @see https://developer.mozilla.org/fr/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
  *
  * @package	Coercive\App\Utility
  * @author	Anthony Moral <contact@coercive.fr>
